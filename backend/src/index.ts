7db99e65664595e2430016298ec6332a79c5aa67
RNanWP <renan92011@hotmail.com>
2026-02-20 23:09:02 -0300

    Fix: Tentando resolver algo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,3 +13,18 @@ const port = Number(process.env.PORT || 3333);
 app.listen(port, () => {
   console.log(`Backend rodando em http://localhost:${port}`);
 });
+
+app.use("/api", routes);
+
+app.use((err: any, _req: any, res: any, _next: any) => {
+  console.error("API Error:", err);
+
+  // Zod
+  if (err?.name === "ZodError") {
+    return res
+      .status(400)
+      .json({ error: "ValidationError", issues: err.issues });
+  }
+
+  return res.status(500).json({ error: "InternalServerError" });
+});
